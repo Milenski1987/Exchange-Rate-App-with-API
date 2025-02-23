9492982f4ffb97758abd24e8e3463e0b67c90093
Milen Nikolov <milenski1987@icloud.com>
2025-02-23 23:19:52 +0200

    improvements
    -added check for input validity
    -changed background for main window

--- exchange_app.py
+++ exchange_app.py
@@ -3,13 +3,30 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk
+import os
+import sys
 
-#read file with currencies data
-with open("currencies_data", "r") as data_currency:
-    supported_currency = data_currency.readlines()
-    available_currencies = [currency.strip("\n") for currency in supported_currency]
 
-def exchange(current_currency:str,current_currency_amount: int, wanted_currency:str) -> str:
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+def check_input_validity(func):
+    def wrapper(current_currency_amount, *args, **kwargs):
+        if not isinstance(current_currency_amount, (int,float)):
+            raise ValueError("Amount must be valid number!")
+        if current_currency_amount <= 0:
+            raise ValueError("Amount must be positive number!")
+        return func(current_currency_amount, *args, **kwargs)
+    return wrapper
+
+
+@check_input_validity
+def exchange(current_currency_amount: float,current_currency:str, wanted_currency:str) -> str:
     # Making our request and get information
     url = f'https://v6.exchangerate-api.com/v6/66c0148aeceee70c30376892/latest/{current_currency.split()[1]}'
     response = requests.get(url)
@@ -19,20 +36,29 @@ def exchange(current_currency:str,current_currency_amount: int, wanted_currency:
             f"For {current_currency_amount} {current_currency.split()[1]}\n "
             f"you will receive {current_result:.3f}{wanted_currency.split()[1]}\n\n")
 
-def dashboard_rates(first_currency:str , second_currency: str) -> float:
-    # Get information for our dashboard with most common currencies
-    url = f'https://v6.exchangerate-api.com/v6/66c0148aeceee70c30376892/latest/{first_currency}'
-    response = requests.get(url)
-    rate = response.json()["conversion_rates"][second_currency]
-    return rate
 
 def get_exchange_information():
     # main GUI function
-    user_current_currency = input_currency_menu.get()
-    user_wanted_currency = input_receive_menu.get()
-    user_current_currency_amount = float(input_field_entry_amount.get())
-    response = exchange(user_current_currency, user_current_currency_amount, user_wanted_currency)
-    response_field.insert(tk.END, response)
+    try:
+        user_current_currency = input_currency_menu.get()
+        user_wanted_currency = input_receive_menu.get()
+        user_current_currency_amount = float(input_field_entry_amount.get())
+        response = exchange( user_current_currency_amount,user_current_currency, user_wanted_currency)
+        response_field.insert(tk.END, response)
+    except ValueError as e:
+        response_field.insert(tk.END, e)
+
+
+def dashboard_rates(first_currency:str) -> float:
+    # Get information for our dashboard with most common currencies
+    url = f'https://v6.exchangerate-api.com/v6/66c0148aeceee70c30376892/latest/{first_currency}'
+    response = requests.get(url)
+    rate_usd = response.json()["conversion_rates"]["USD"]
+    rate_gbp = response.json()["conversion_rates"]["GBP"]
+    rate_chf = response.json()["conversion_rates"]["CHF"]
+    rate_bgn = response.json()["conversion_rates"]["BGN"]
+    rate_cny = response.json()["conversion_rates"]["CNY"]
+    return rate_usd, rate_gbp, rate_chf, rate_bgn, rate_cny
 
 
 def information_pop_up():
@@ -41,10 +67,9 @@ def information_pop_up():
     informative_screen.title("Detailed information")
     imagebox = tk.Label(informative_screen)
     imagebox.pack()
-    image = ImageTk.PhotoImage(file="currency_codes.png")
+    image = ImageTk.PhotoImage(file=resource_path("currency_codes.png"))
     imagebox.config(image=image)
     imagebox.image = image
-
 
 
 def main():
@@ -62,13 +87,18 @@ def main():
     print(result)
 
 
+# read file with currencies data
+with open(resource_path("currencies_data.txt"), "r") as data_currency:
+    supported_currency = data_currency.readlines()
+    available_currencies = [currency.strip("\n") for currency in supported_currency]
+
 #create GUI
 root = tk.Tk()
 root.title("Exchange rate App")
 root.geometry("1020x690")
 
 #define image
-bg = PhotoImage(file="gettyimages-609801990.png")
+bg = PhotoImage(file=resource_path("official_background.png"))
 
 #create canvas
 my_canvas = tk.Canvas(root, width=1020, height=680)
@@ -77,13 +107,10 @@ my_canvas.pack(fill="both", expand=True)
 #set image in canvas
 my_canvas.create_image(0,0,image = bg, anchor="nw")
 
-#create labels
-input_field_label_type = tk.Label(my_canvas,width = 34,bg = "light yellow",fg="black",text="Choose the currency you want to exchange here: ")
-input_field_label_receive = tk.Label(my_canvas,width = 34,bg = "light yellow",fg="black", text="Choose the currency you want to receive here:")
-
-#arrange labels
-input_field_label_type.place(x = 290, y = 250)
-input_field_label_receive.place(x= 290, y = 310)
+#better way for labels
+my_canvas.create_text(450,260 , text="Choose the currency you want to exchange here: ",font=("Arial", 15), fill="black")
+my_canvas.create_text(450,320 , text="Choose the currency you want to receive here:",font=("Arial", 15), fill="black")
+my_canvas.create_text(450,385 , text="Currency amount you want to exchange: ",font=("Arial", 15), fill="black")
 
 #create menus
 input_currency_menu = StringVar(my_canvas)
@@ -99,14 +126,9 @@ receive_menu.config(bg="light blue",fg="black",width=7)
 input_menu.place(x= 400, y= 275)
 receive_menu.place(x= 400, y = 335)
 
-#create amount entry label and arrange it
-input_field_label_amount = tk.Label(my_canvas,width = 34,bg = "light yellow",fg="black", text="Currency amount you want to exchange:")
-input_field_label_amount.place(x= 290, y = 375)
-
 #create amount entry field and arrange it
 input_field_entry_amount = tk.Entry(my_canvas,justify='center', bg="light blue", fg="black", width=10)
 input_field_entry_amount.place(x= 400, y= 400)
-
 
 #create exchange button and arrange it
 style = ttk.Style()
@@ -114,31 +136,26 @@ style.configure("TButton", padding=0,bordercolor="lightblue", background="#ADD8E
 exchange_button = ttk.Button(root,width= 15, text ="Exchange", command=lambda: get_exchange_information())
 exchange_button.place(x = 360, y = 430)
 
-
 #create app response field and arrange it
 response_field = tk.Text(my_canvas, bg="light blue" , fg="black", width=44, height= 11)
 response_field.place(x = 290, y = 470)
 
 #create and arrange dashboard with most common currencies
-dashboard_text = tk.Text(my_canvas, bg="light yellow", fg="black", font="Arial",width= 25, height=12)
-dashboard_text.insert(tk.END, f"Most common currencies rates:\n\n🇧🇬BGN to 🇪🇺EUR ->  {dashboard_rates("BGN", "EUR"):.3f} EUR")
-dashboard_text.insert(tk.END, f"\n🇪🇺EUR to 🇧🇬BGN->  {dashboard_rates("EUR", "BGN"):.3f} BGN")
-dashboard_text.insert(tk.END, f"\n🇧🇬BGN to 🇺🇸USD ->  {dashboard_rates("BGN", "USD"):.3f} USD")
-dashboard_text.insert(tk.END, f"\n🇺🇸USD to 🇧🇬BGN ->  {dashboard_rates("USD", "BGN"):.3f} BGN")
-dashboard_text.insert(tk.END, f"\n🇧🇬BGN to 🇬🇧GBP->  {dashboard_rates("BGN", "GBP"):.3f} GBP")
-dashboard_text.insert(tk.END, f"\n🇬🇧GBP to 🇧🇬BGN->  {dashboard_rates("GBP", "BGN"):.3f} BGN")
-dashboard_text.insert(tk.END, f"\n🇺🇸USD to 🇪🇺EUR ->  {dashboard_rates("USD", "EUR"):.3f} USD")
-dashboard_text.insert(tk.END, f"\n🇪🇺EUR to 🇺🇸USD->  {dashboard_rates("EUR", "USD"):.3f} EUR")
-dashboard_text.insert(tk.END, f"\n🇪🇺EUR to 🇬🇧GBP->  {dashboard_rates("EUR", "GBP"):.3f} GBP")
-dashboard_text.insert(tk.END, f"\n🇬🇧GBP to 🇪🇺EUR->  {dashboard_rates("GBP", "EUR"):.3f} EUR")
-dashboard_text.place(x= 350, y = 20)
-
+dashboard_text = tk.Text(my_canvas, bg="light yellow", fg="black", font="Arial",width= 28, height=8)
+rate_usd, rate_gbp, rate_chf, rate_bgn, rate_cny = dashboard_rates("EUR")
+dashboard_text.insert(tk.END, f"Common currencies rates for 🇪🇺Euro\n.                         We Buy | We Sell")
+dashboard_text.insert(tk.END, f"\n 🇺🇸US Dollar        {rate_usd:.3f} | {(rate_usd + 0.012):.3f}")
+dashboard_text.insert(tk.END, f"\n 🇬🇧British Pound  {rate_gbp:.3f} | {(rate_gbp + 0.011):.3f}")
+dashboard_text.insert(tk.END, f"\n 🇨🇭Swiss Franc    {rate_chf:.3f} | {(rate_chf + 0.012):.3f}")
+dashboard_text.insert(tk.END, f"\n 🇧🇬Bulgarian Lev  {rate_bgn:.3f} | {(rate_bgn + 0.010):.3f}")
+dashboard_text.insert(tk.END, f"\n 🇨🇳Chinese yuan  {rate_cny:.3f} | {(rate_cny + 0.015):.3f}")
+dashboard_text.place(x= 340, y = 20)
 
 #create button for pop-up informative window
-information_window_label = tk.Label(my_canvas,width = 27,bg = "light yellow",fg="black",text="For more detailed currency information: ")
-information_window_label.place(x= 660, y= 35)
+my_canvas.create_text(800,53 , text="For more detailed currency information: ",font=("Arial", 15), fill="black")
 informative_button = ttk.Button(my_canvas, width=15, text = " Click Here!", command= lambda: information_pop_up() )
 informative_button.place(x= 705, y = 65)
+
 
 root.mainloop()
 main()
